fbb7acbb8ed718b7a379f5d4b196594bb1b35aa0
Shaye <shayelee25@gmail.com>
2026-01-28 14:28:34 -0500

    Write rough draft of psuedocode for game

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Hello, World!")
+
+Create function getComputerChoice
+    function returns string values: "rock", "paper", or "scissors"
+Test function with console.log
+
+Use prompt to get user's input
+Create function getHumanChoice
+Test function with console.log
+
+Create variable humanScore
+Create variable computerScore
+Initialize variables with the value of 0
+
+Create function playRound
+Define two parameters for playRound: humanChoice and computerChoice
+    These parameters will take the human and computer choices as arguments
+Make function humanChoice case-insensitive
+console.log should return a string value, such as "You lose! Paper beats Rock."
+Increment the humanScore or computerScore variables based on the round winner
+
+Create function playGame
+Move the playRound function and score variables so that they're declared inside of the playGame function
+Play 5 rounds by calling playRound 5 times
+Score variables track with each round
+Winner is declared at the end of the game
+
