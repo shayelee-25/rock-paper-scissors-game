e9f37b6a8c5038f12dd968f798af95b535d1df0d
Shaye <shayelee25@gmail.com>
2026-02-03 18:31:05 -0500

    Add 'for' loop in playGame, simplify functions

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-// logic to collect a randomized choice from the computer
-let computerInput = Math.floor(Math.random() * 100) + 1;
-function getComputerChoice () {
-    if (computerInput <= 33) {
-       return "rock";
-    } else if (computerInput > 33 && computerInput <= 66) {           
+// function to collect a randomized choice from the computer
+const input = ["rock", "paper", "scissors"];
+let computerInput = input[Math.floor(Math.random() * input.length)];function getComputerChoice () {
+    if (computerInput === "rock") {
+        return "rock";
+    } else if (computerInput === "paper") {           
        return "paper"; 
     } 
-      return "scissors";
+       return "scissors";
 } 
-//console.log(getComputerChoice())
 
-// prompt to collect user's choice
-const userInput = prompt("Please type your choice: rock, paper, scissors").toLowerCase();
-function getHumanChoice() {
-    return userInput;
-} 
-//console.log(getHumanChoice())
+// scoring variables
+let humanScore = 0; 
+let computerScore = 0;
 
-
-// variables to correlate collected input and parameters for playRound
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-
-// logic for a single round of the game
+// single round of the game
 function playRound(humanChoice, computerChoice) {
+    let humanInput = prompt("Please enter your choice: rock, paper, or scissors").toLowerCase();
+    humanChoice = humanInput;
+    computerChoice = getComputerChoice();
+    console.log(humanInput)
+    console.log(getComputerChoice())
     if ( 
         (humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
         humanScore += 1;
@@ -32,57 +28,31 @@ function playRound(humanChoice, computerChoice) {
         (humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
         computerScore += 1;
         return "Oh no, you lost this round!";
-    } 
+    }
         return "Oops! You tied, try again!";
 } 
-//console.log(playRound(humanChoice, computerChoice))
 
-// scoring variables
-let humanScore = 0; 
-let computerScore = 0;
-
-
-function playGame() {
-    for (i = 0; i < 5; i++ ) { 
-       console.log(getHumanChoice())
-       console.log(getComputerChoice())
-       console.log(playRound());
-       console.log(`User: ${humanScore}`, `Computer: ${computerScore}`)
-    }
-    displayResults();
-} 
-//console.log(playGame())
-
-// logic to display results at the end of the game
+// display results at the end of the game
 function displayResults() {
     if (humanScore > computerScore) {
-        console.log(`You won the game, ${humanScore} - ${computerScore}!`);
-    } else (humanScore < computerScore) 
-        console.log(`You lost ${humanScore} - ${computerScore}, better luck next time!`);
-    } 
+        return `You won the game, ${humanScore} - ${computerScore}!`;
+    } else if (humanScore < computerScore) {
+        return `You lost ${humanScore} - ${computerScore}, better luck next time!`;
+    } else {
+        return "No winner this game, refresh page to play again!";
+    }
+}
+
+// sets game to be a total of 5 rounds
+function playGame() {
+    const totalRounds = 5;
+    for (let round = 1; round <= totalRounds; round++) {
+        console.log(`-----Round ${round}-----`)
+        console.log(playRound())
+        console.log(`User: ${humanScore}`, `Computer: ${computerScore}`)
+    }
+    console.log(displayResults())
+}
+
+// starts game
 playGame();
-
-/* Create function getComputerChoice
-    function returns string values: "rock", "paper", or "scissors"
-Test function with console.log
-
-Use prompt to get user's input
-Create function getHumanChoice
-Test function with console.log
-
-Create variable humanScore
-Create variable computerScore
-Initialize variables with the value of 0
-
-Create function playRound
-Define two parameters for playRound: humanChoice and computerChoice
-    These parameters will take the human and computer choices as arguments
-Make function humanChoice case-insensitive
-console.log should return a string value, such as "You lose! Paper beats Rock."
-Increment the humanScore or computerScore variables based on the round winner
-
-Create function playGame
-Move the playRound function and score variables so that they're declared inside of the playGame function
-Play 5 rounds by calling playRound 5 times
-Score variables track with each round
-Winner is declared at the end of the game*/
